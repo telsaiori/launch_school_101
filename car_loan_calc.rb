1090f2a82b8b49889af26475d75356fd4685e1f1
telsa <telsa@shantianlicaide-MacBook-Air.local>
2016-07-11 16:30:10 +0800

    fix type and rubocop issue

--- car_loan_calc.rb
+++ car_loan_calc.rb
@@ -2,8 +2,8 @@ def prompt(message)
   puts "=>#{message}"
 end
 
-def vaild_number?(number)
-  number.empty? || number.to_f < 0 || number.to_i == 0
+def invaild_number?(number)
+  number.empty? || number.to_f < 0 || number.to_f == 0.0
 end
 
 prompt("Welcome to Mortgage Calculator!")
@@ -12,7 +12,7 @@ loop do
   loop do
     prompt("What is the the loan amount")
     amount = gets.chomp
-    if vaild_number?(amount)
+    if invaild_number?(amount)
       prompt("Please enter valid number")
     else
       break
@@ -21,10 +21,10 @@ loop do
 
   apr = ""
   loop do
-    prompt("what is your What is your APR?")
+    prompt("What is your APR?")
     prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
     apr = gets.chomp
-    if vaild_number?(apr)
+    if invaild_number?(apr)
       prompt("Please enter the valid APR")
     else
       break
@@ -35,7 +35,7 @@ loop do
   loop do
     prompt("what is your the loan duration in years")
     duration = gets.chomp
-    if vaild_number?(duration)
+    if invaild_number?(duration)
       prompt("Please enter valid loan duration")
     else
       break
@@ -48,12 +48,11 @@ loop do
   monthly_payment = amount.to_f *
                     (monthly_interest * (1 + monthly_interest)**months) /
                     ((1 + monthly_interest)**months - 1)
-  prompt("Your monthly payment is #{format('%.3f', monthly_payment)}")
+  prompt("Your monthly payment is $#{format('%.2f', monthly_payment)}")
   prompt("Another calculation? (Y for yes)")
   calc = gets.chomp
-  unless calc.casecmp('y') == 0
-    prompt("Thanks for using")
-    prompt("Bye-bye")
-    break
-  end
+  next if calc.casecmp('y') == 0
+  prompt("Thanks for using")
+  prompt("Bye-bye")
+  break
 end
