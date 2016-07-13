f01ea46d6b02e8209114941c0750547bffb99385
telsa <telsa@shantianlicaide-MacBook-Air.local>
2016-07-13 16:54:30 +0800

    move the main if/else statement to a method

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -20,41 +20,45 @@ def display_result(player_score, computer_score)
   end
 end
 
+def result_record(player_choice, computer_choice, score)
+  if WIN_CONDITION[player_choice.to_sym].include?(computer_choice)
+    score[:player] += 1
+    prompt("You Win!!")
+  elsif INPUT[player_choice.to_sym] == computer_choice
+    prompt("Tie!!")
+  else
+    score[:computer] += 1
+    prompt("You Lose!!")
+  end
+end
+
 loop do
   player_choice = ''
   computer_choice = ''
-  player_score = 0
-  computer_score = 0
-  tie = 0
+  score = { player: 0, computer: 0 }
   prompt("Get 5 point to win")
   loop do
-    if player_score < 5 && computer_score < 5
-      prompt("Choose one: r as rock, p as paper, s as scissors, v as spock, l as lizard")
-      player_choice = gets.chomp
+    if score[:player] < 5 && score[:computer] < 5
+      prompt("Your score: #{score[:player]}," \
+        "Computer's score: #{score[:computer]}")
+      prompt("Choose one: r as rock, p as paper," \
+        " s as scissors, v as spock, l as lizard")
+      player_choice = gets.chomp.downcase
       computer_choice = VAILD_INPUT.sample
       if INPUT.key?(player_choice.to_sym)
-        prompt("You choose #{INPUT[player_choice.to_sym]}, computer choose #{computer_choice}")
-        if WIN_CONDITION[player_choice.to_sym].include?(computer_choice)
-          player_score += 1
-          prompt("You Win!!")
-        elsif INPUT[player_choice.to_sym] == computer_choice
-          tie += 1
-          prompt("Tie!!")
-        else
-          computer_score += 1
-          prompt("You Lose!!")
-          p computer_score
-        end
+        prompt("You choose #{INPUT[player_choice.to_sym]}," \
+          "computer choose #{computer_choice}")
+        result_record(player_choice, computer_choice, score)
       else
-        prompt("Please enter r as rock, p as paper, s as scissors, v as spock, l as lizard")
+        prompt("Please enter r as rock, p as paper,"  \
+          "s as scissors, v as spock, l as lizard")
       end
     else
       break
     end
   end
 
-  display_result(player_score, computer_score)
-
+  display_result(score[:player], score[:computer])
   prompt("Do you want to play again? (Y for yes)")
   input = gets.chomp
   unless input.casecmp('y') == 0
